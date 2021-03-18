2da6fe8501e604d5f1f2ea2cd79ffdae57ca7c21
Adrienne Stilp <amstilp@uw.edu>
2021-03-18 14:51:32 -0700

    Add basic function for calculating LD for a pair of variants

--- LDcompute/R/compute-ld.R
+++ LDcompute/R/compute-ld.R
@@ -1,7 +1,25 @@
-compute_ld <- function() {}
+compute_ld <- function(gds, variant_include_1, variant_include_2 = NULL) {
+
+  # Checks - to be written.
+  # Choose which method to call - to be written.
+
+  # Compute LD for a pair of variants.
+  .compute_ld_one_to_one(gds, variant_include_1, variant_include_2)
+
+}
 
 # Computes LD between a pair of variants.
-.compute_ld_one_to_one <- function() {}
+.compute_ld_one_to_one <- function(gds, variant_include_1, variant_include_2, method = "composite") {
+
+  ld <- snpgdsLDMat(gds, snp.id = c(variant_include_1, variant_include_2),
+                    slide = -1, verbose = FALSE)
+
+  tibble::tibble(
+    variant.id.1 = variant_include_1,
+    variant.id.2 = variant_include_2,
+    ld_composite = ld$LD[1,2]
+  )
+}
 
 # Computes LD between one variant and a set of other variants.
 .compute_ld_one_to_many <- function() {}
--- LDcompute/tests/testthat/test-compute-ld-one-to-one.R
+++ LDcompute/tests/testthat/test-compute-ld-one-to-one.R
@@ -1,1 +1,41 @@
-test_that("calculate LD between two variants", {})
+test_that("works normally", {
+  gds <- local_gds()
+  var1 <- 1
+  var2 <- 2
+  ld <- compute_ld(gds, var1, var2)
+
+  expect_equal(names(ld), c("variant.id.1", "variant.id.2", "ld_composite"))
+  expect_equal(nrow(ld), 1)
+  expect_equal(ld$variant.id.1, var1)
+  expect_equal(ld$variant.id.2, var2)
+  expect_true(is.numeric(ld$ld_composite))
+
+  # Check against the function directly.
+  chk <- snpgdsLDMat(gds, snp.id = c(var1, var2), slide = -1, verbose = FALSE)
+  expect_equal(unname(ld$ld_composite), chk$LD[1,2])
+})
+
+test_that("returns ids in the correct order", {
+  gds <- local_gds()
+  var1 <- 2
+  var2 <- 1
+  ld <- compute_ld(gds, var1, var2)
+
+  expect_equal(names(ld), c("variant.id.1", "variant.id.2", "ld_composite"))
+  expect_equal(nrow(ld), 1)
+  expect_equal(ld$variant.id.1, var1)
+  expect_equal(ld$variant.id.2, var2)
+  expect_true(is.numeric(ld$ld_composite))
+
+  # Check against the function directly.
+  chk <- snpgdsLDMat(gds, snp.id = c(var1, var2), slide = -1, verbose = FALSE)
+  expect_equal(unname(ld$ld_composite), chk$LD[1,2])
+})
+
+test_that("missing data", {})
+
+test_that("multiallelic variants", {})
+
+test_that("multiple methods", {})
+
+test_that("different chromosomes", {})
