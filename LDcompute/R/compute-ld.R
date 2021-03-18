97b8660cf9212580e903f25723d161406b9f7db7
Adrienne Stilp <amstilp@uw.edu>
2021-03-18 14:00:50 -0700

    Add skeleton R functions and tests

---
+++ LDcompute/R/compute-ld.R
@@ -0,0 +1,10 @@
+compute_ld <- function() {}
+
+# Computes LD between a pair of variants.
+.compute_ld_one_to_one <- function() {}
+
+# Computes LD between one variant and a set of other variants.
+.compute_ld_one_to_many <- function() {}
+
+# Computes LD between all pairs of a set of variants.
+.compute_ld_many_to_many <- function() {}
