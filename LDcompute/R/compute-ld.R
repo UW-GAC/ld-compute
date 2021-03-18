cb6275a14208b60ea423aa75ec8452d122e834d6
Adrienne Stilp <amstilp@uw.edu>
2021-03-18 16:42:47 -0700

    Begin adding roxygen documentation

--- LDcompute/R/compute-ld.R
+++ LDcompute/R/compute-ld.R
@@ -1,3 +1,25 @@
+#' Compute LD among variants
+#'
+#' @param gds A SeqArray GDS file
+#' @param variant_include_1 A vector of variant ids. See details.
+#' @param variant_include_2 A vector of variant.ids. See details.
+#' @param methods A vector of methods to use to calculate LD. See details.
+#'
+#' @details
+#' This function is primarily a wrapper around the SNPRelate snpgdsLDMat function.
+#' It allows LD calculations between a pair of variants and returns results in a consistent format.
+#'
+#' @value
+#' A data frame with the following columns:
+#' * variant.id.1: the first variant id in the pair
+#' * variant.id.2: the second variant id in the pair
+#' * (if `method="composite"``) ld_composite: the LD between variant.id.1 and variant.id.2 calculated using the "composite" method
+#' * (if `method="dprime"``) ld_composite: the LD between variant.id.1 and variant.id.2 calculated using the "dprime" method
+#' * (if `method="corr"``) ld_composite: the LD between variant.id.1 and variant.id.2 calculated using the "corr" method
+#' * (if `method="r"``) ld_composite: the LD between variant.id.1 and variant.id.2 calculated using the "r" method
+#'
+#' @md
+
 compute_ld <- function(gds, variant_include_1, variant_include_2 = NULL, methods = c("composite")) {
 
   # Checks - to be written.
