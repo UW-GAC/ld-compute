18ba6aaaf8697c5cd625000b5893b990f27d3076
Adrienne Stilp <amstilp@uw.edu>
2021-03-18 16:18:02 -0700

    Allow different LD caclulation methods to be specified

--- LDcompute/R/compute-ld.R
+++ LDcompute/R/compute-ld.R
@@ -1,24 +1,38 @@
-compute_ld <- function(gds, variant_include_1, variant_include_2 = NULL) {
+compute_ld <- function(gds, variant_include_1, variant_include_2 = NULL, methods = c("composite")) {
 
   # Checks - to be written.
+
+  # Check that method is allowed
+
+  allowed_methods <- c("composite", "dprime", "corr", "r")
+  if (!(methods %in% allowed_methods)) {
+    msg <- sprintf("method is not in set of allowed methods: %s",
+                   paste(allowed_methods, collapse = ", "))
+    stop(msg)
+  }
+
   # Choose which method to call - to be written.
 
   # Compute LD for a pair of variants.
-  .compute_ld_one_to_one(gds, variant_include_1, variant_include_2)
+  .compute_ld_one_to_one(gds, variant_include_1, variant_include_2, methods = methods)
 
 }
 
 # Computes LD between a pair of variants.
-.compute_ld_one_to_one <- function(gds, variant_include_1, variant_include_2, method = "composite") {
+.compute_ld_one_to_one <- function(gds, variant_include_1, variant_include_2, methods = "composite") {
 
+  # For variant.ids with multiple alternate alleles, I think that snpgdsLDMat
+  # just uses the ref dosage to calculate LD.
   ld <- snpgdsLDMat(gds, snp.id = c(variant_include_1, variant_include_2),
-                    slide = -1, verbose = FALSE)
+                    method = methods, slide = -1, verbose = FALSE)
 
-  tibble::tibble(
+  dat <- tibble::tibble(
     variant.id.1 = variant_include_1,
     variant.id.2 = variant_include_2,
-    ld_composite = ld$LD[1,2]
+    ld = ld$LD[1,2]
   )
+  names(dat)[names(dat) == "ld"] <- sprintf("ld_%s", methods)
+  dat
 }
 
 # Computes LD between one variant and a set of other variants.
--- LDcompute/tests/testthat/test-compute-ld-one-to-one.R
+++ LDcompute/tests/testthat/test-compute-ld-one-to-one.R
@@ -64,6 +64,49 @@ test_that("multiallelic variants", {
   expect_true(!is.na(ld$ld_composite))
 })
 
-test_that("multiple methods", {})
+test_that("different methods", {
+  # Use a different method to calculate LD.
+  gds <- local_gds()
+  var1 <- 1
+  var2 <- 2
 
-test_that("different chromosomes", {})
+  ld <- compute_ld(gds, var1, var2, methods = "composite")
+  expect_equal(names(ld), c("variant.id.1", "variant.id.2", "ld_composite"))
+  expect_equal(nrow(ld), 1)
+  expect_equal(ld$variant.id.1, var1)
+  expect_equal(ld$variant.id.2, var2)
+  chk <- snpgdsLDMat(gds, snp.id = c(var1, var2), method = "composite", slide = -1, verbose = FALSE)
+  expect_equal(unname(ld$ld_composite), chk$LD[1,2])
+
+  ld <- compute_ld(gds, var1, var2, methods = "dprime")
+  expect_equal(names(ld), c("variant.id.1", "variant.id.2", "ld_dprime"))
+  expect_equal(nrow(ld), 1)
+  expect_equal(ld$variant.id.1, var1)
+  expect_equal(ld$variant.id.2, var2)
+  chk <- snpgdsLDMat(gds, snp.id = c(var1, var2), method = "dprime", slide = -1, verbose = FALSE)
+  expect_equal(unname(ld$ld_dprime), chk$LD[1,2])
+
+  ld <- compute_ld(gds, var1, var2, method = "corr")
+  expect_equal(names(ld), c("variant.id.1", "variant.id.2", "ld_corr"))
+  expect_equal(nrow(ld), 1)
+  expect_equal(ld$variant.id.1, var1)
+  expect_equal(ld$variant.id.2, var2)
+  chk <- snpgdsLDMat(gds, snp.id = c(var1, var2), method = "corr", slide = -1, verbose = FALSE)
+  expect_equal(unname(ld$ld_corr), chk$LD[1,2])
+
+  ld <- compute_ld(gds, var1, var2, method = "r")
+  expect_equal(names(ld), c("variant.id.1", "variant.id.2", "ld_r"))
+  expect_equal(nrow(ld), 1)
+  expect_equal(ld$variant.id.1, var1)
+  expect_equal(ld$variant.id.2, var2)
+  chk <- snpgdsLDMat(gds, snp.id = c(var1, var2), method = "r", slide = -1, verbose = FALSE)
+  expect_equal(unname(ld$ld_r), chk$LD[1,2])
+
+  # Method not allowed.
+  expect_error(compute_ld(gds, var1, var2, method = "foo"), "allowed methods")
+
+})
+
+test_that("different chromosomes", {
+  fail("what do we want to happen?")
+})
