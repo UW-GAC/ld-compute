c2799288db3c9df2e5861c596ac63894feee19e4
Adrienne Stilp <amstilp@uw.edu>
2021-03-19 14:14:29 -0700

    Add skeleton file for "other" tests
    These tests will test input checking functionality and aren't
    testing the specific one-to-one, one-to-many, or many-to-many cases.

---
+++ LDcompute/tests/testthat/test-compute-ld-other.R
@@ -0,0 +1,13 @@
+# Tests of input checking or other standard checks regardless of which calculation is being done.
+
+test_that("multiple variant_include_1 and non-null variant_id_2", {
+  skip("write this test")
+})
+
+test_that("non-existing sample_include", {
+  skip("write this test")
+})
+
+test_that("non-existing variant_include", {
+  skip("write this test")
+})
