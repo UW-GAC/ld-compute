ecdf256c663c317689ff8fda49cf710b7a6ac405
Adrienne Stilp <amstilp@uw.edu>
2021-03-22 15:53:01 -0700

    Update docs for separate functions

--- LDcompute/R/compute-ld.R
+++ LDcompute/R/compute-ld.R
@@ -1,20 +1,13 @@
-#' Compute LD among variants
+#' Compute LD between a pair of variants
 #'
 #' @param gds A SeqArray GDS file
-#' @param variant_include_1 A vector of variant ids. See details.
-#' @param variant_include_2 A vector of variant.ids. See details.
-#' @param methods A vector of methods to use to calculate LD. See details.
+#' @param variant_id_1 First variant id in the pair
+#' @param variant_id_2 Second variant id in the pair
+#' @param methods Character vector of methods to use to calculate LD. Can be any of `"composite"`, `"corr"`, `"r"`, `"dprime"`.
 #' @param sample_include A vector of sample.ids to use for LD calculation.
 #'
 #' @details
-#' This function is primarily a wrapper around the SNPRelate snpgdsLDMat function.
-#' It allows LD calculations between a pair of variants, one variant and a set of other variants, and all pairs within a set of variants,
-#' and returns results in a consistent format.
-#'
-#' The type of LD calculation is determined by how `variant_include_1` and `variant_include_2` are set.
-#' * a pair of variants: set `variant.id.1` to one variant.id in the pair and `variant.id.2` to the other variant.id in the pair.
-#' * one variant and a set of other variants: set `variant.id.1` to the variant.id and `variant.id.2` to the variant.ids for which LD should be calculated with respect to `variant.id.1`
-#' * all pairs in a set of variants: set `variant.id.1` to a vector of variant ids and leave `variant.id.2` as `NULL`.
+#' This function computes the LD between two variants using \code{snpgdsLDMat} using the specified methods.
 #'
 #' @return
 #' A data frame with the following columns:
@@ -32,16 +25,17 @@
 #'
 #' @importFrom SNPRelate snpgdsLDMat
 #' @importFrom dplyr left_join %>% filter .data
-#' @importFrom tidyr %>%
-#' @importFrom tibble tibble %>%
-
-
-compute_ld_pair <- function (gds, variant_include_1, variant_include_2, methods = "composite", sample_include = NULL) {
+#'
+#' @seealso
+#' * [compute_ld_pair()] computes LD between a pair of variants
+#' * [compute_ld_set()] computes LD between all pairs of a set of variants
+#' * [compute_ld_index()] computes LD between one variant and a set of other variants
+compute_ld_pair <- function (gds, variant_id_1, variant_id_2, methods = "composite", sample_include = NULL) {
 
     # Checks - to be written.
     .check_ld_methods(methods)
 
-    variant_include <- unique(c(variant_include_1, variant_include_2))
+    variant_include <- unique(c(variant_id_1, variant_id_2))
 
     .check_ld_multiallelic(gds, variant_include)
 
@@ -50,7 +44,7 @@ compute_ld_pair <- function (gds, variant_include_1, variant_include_2, methods 
     for (method in methods) {
       # Calculate ld between all pairs of variants provided.
       dat <- .compute_ld_matrix(gds, variant_include, method, sample_include = sample_include) %>%
-          filter(.data$variant.id.1 == variant_include_1, .data$variant.id.2 == variant_include_2)
+          filter(.data$variant.id.1 == variant_id_1, .data$variant.id.2 == variant_id_2)
       res_list[[method]] <- dat
     }
 
@@ -68,6 +62,36 @@ compute_ld_pair <- function (gds, variant_include_1, variant_include_2, methods 
 
 }
 
+
+#' Compute LD among all pairs of a set of variants
+#'
+#' @inheritParams compute_ld_pair
+#' @param variant_include THe variant.ids for which to calculate ld
+#'
+#' @details
+#' This function computes the LD between all pairs in a set of variants using \code{snpgdsLDMat} using the specified methods.
+#'
+#' @return
+#' A data frame with the following columns:
+#' * variant.id.1: the first variant id in the pair
+#' * variant.id.2: the second variant id in the pair
+#' * One column for each method specified giving the LD btween `variant.id.1` and `variant.id.2` using the specified method:
+#'     * if `methods` contains `"composite"`: `ld_composite`
+#'     * if `methods` contains `dprime`: `"ld_dprime"`
+#'     * if `methods` contains `"corr"`: `ld_corr`
+#'     * if `methods` contains `"r"`: `ld_r`
+#'
+#' Each pair of variants only has one record in the data frame.
+#'
+#' @md
+#'
+#' @importFrom SNPRelate snpgdsLDMat
+#' @importFrom dplyr left_join %>% filter .data
+#'
+#' @seealso
+#' * [compute_ld_pair()] computes LD between a pair of variants
+#' * [compute_ld_set()] computes LD between all pairs of a set of variants
+#' * [compute_ld_index()] computes LD between one variant and a set of other variants
 compute_ld_set <- function(gds, variant_include, methods = "composite", sample_include = NULL) {
 
   # Checks - to be written.
@@ -100,21 +124,52 @@ compute_ld_set <- function(gds, variant_include, methods = "composite", sample_i
 
 }
 
-compute_ld_index <- function (gds, reference_variant, variant_include, methods = "composite", sample_include = NULL) {
+
+#' Compute LD among one index variant and a set of other variants
+#'
+#' @inheritParams compute_ld_pair
+#' @param index_variant_id The variant.id of the index variant
+#' @param other_variant_ids A vector of variant ids to calculate LD with the `index_variant_id`
+#'
+#' @details
+#' This function computes the LD between `index_variant_id` and the variant ids specified in `other_variant_ids`.
+#'
+#' @return
+#' A data frame with the following columns:
+#' * variant.id.1: the first variant id in the pair
+#' * variant.id.2: the second variant id in the pair
+#' * One column for each method specified giving the LD btween `variant.id.1` and `variant.id.2` using the specified method:
+#'     * if `methods` contains `"composite"`: `ld_composite`
+#'     * if `methods` contains `dprime`: `"ld_dprime"`
+#'     * if `methods` contains `"corr"`: `ld_corr`
+#'     * if `methods` contains `"r"`: `ld_r`
+#'
+#' Each pair of variants only has one record in the data frame.
+#'
+#' @md
+#'
+#' @importFrom SNPRelate snpgdsLDMat
+#' @importFrom dplyr left_join %>% filter .data
+#'
+#' @seealso
+#' * [compute_ld_pair()] computes LD between a pair of variants
+#' * [compute_ld_set()] computes LD between all pairs of a set of variants
+#' * [compute_ld_index()] computes LD between one variant and a set of other variants
+compute_ld_index <- function (gds, index_variant_id, other_variant_ids, methods = "composite", sample_include = NULL) {
 
   # Checks - to be written.
   .check_ld_methods(methods)
 
-  all_variants <- unique(c(reference_variant, variant_include))
+  variant_include <- unique(c(index_variant_id, other_variant_ids))
 
-  .check_ld_multiallelic(gds, all_variants)
+  .check_ld_multiallelic(gds, variant_include)
 
   res_list <- list()
   for (method in methods) {
     # Calculate ld between all pairs of variants provided.
-    dat <- .compute_ld_matrix(gds, all_variants, method, sample_include = sample_include) %>%
+    dat <- .compute_ld_matrix(gds, variant_include, method, sample_include = sample_include) %>%
       filter(
-        .data$variant.id.1 == reference_variant, .data$variant.id.2 %in% variant_include,
+        .data$variant.id.1 == index_variant_id, .data$variant.id.2 %in% other_variant_ids,
         # not with itself.
         .data$variant.id.1 != .data$variant.id.2
       )
@@ -135,6 +190,7 @@ compute_ld_index <- function (gds, reference_variant, variant_include, methods =
 }
 
 # Helper functions.
+# Compute LD across a set of variants
 .compute_ld_matrix <- function(gds, variant_include, method, methods = "composite", sample_include = NULL) {
   # Calculate ld between all pairs of variants provided.
   ## This will be memory intensive if calculating LD for many variant.ids.
@@ -155,6 +211,7 @@ compute_ld_index <- function (gds, reference_variant, variant_include, methods =
   dat
 }
 
+# Check that a specified LD method is allowed
 .check_ld_methods <- function(methods) {
   ## Check that method is allowed
   allowed_methods <- c("composite", "dprime", "corr", "r")
@@ -165,6 +222,7 @@ compute_ld_index <- function (gds, reference_variant, variant_include, methods =
   }
 }
 
+# Handle multiallelic variants
 .check_ld_multiallelic <- function(gds, variant_include){
   # Check for multiallelic variants.
   seqSetFilter(gds, variant.id = variant_include, verbose = FALSE)
